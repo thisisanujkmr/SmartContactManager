32fd7e26dc0212ea7fb98bf87b5059abebb49997
thisisanujkmr <113297897+thisisanujkmr@users.noreply.github.com>
2022-09-11 20:20:14 +0530

    Add files via upload

---
+++ src/main/java/com/smart/controller/HomeController.java
@@ -0,0 +1,103 @@
+package com.smart.controller;
+
+import javax.naming.spi.DirStateFactory.Result;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.smart.dao.UserRepository;
+import com.smart.entities.Contact;
+import com.smart.entities.User;
+import com.smart.helper.Message;
+
+@Controller
+public class HomeController {
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder; 
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@GetMapping("/")
+	public String home(Model model) {
+		model.addAttribute("title", "Home- Smart Contact Manager");
+		return "home";
+	}
+	
+	@GetMapping("/about")
+	public String about(Model model) {
+		model.addAttribute("title", "About- Smart Contact Manager");
+		return "about";
+	}
+	
+	@GetMapping("/signup")
+	public String signup(Model model) {
+		model.addAttribute("title", "Register- Smart Contact Manager");
+		model.addAttribute("user", new User());
+		return "signup";
+	}
+	
+	//handler for registering user
+	
+	@PostMapping("/do_register")
+	public String registerUser(@Valid @ModelAttribute("user") User user,BindingResult res, @RequestParam(value="agreement", defaultValue = "false")
+	boolean agreement, Model model, HttpSession session) {
+		
+		try {
+			
+			if(!agreement) {
+				System.out.println("You have not agreed the terms and conditions");
+				throw new Exception("You have not agreed the terms and conditions");
+			}
+			
+			if(res.hasErrors()) {
+				System.out.println("ERROR "+ res.toString());
+				model.addAttribute("user", user);
+				return "signup";
+			}
+			
+			user.setRole("ROLE_USER");
+			user.setEnabled(true);
+			user.setImageUrl("Default.png");
+			user.setPassword(passwordEncoder.encode(user.getPassword()));
+			
+			System.out.println("Agreement " + agreement);
+			System.out.println("User "+user);
+			
+			User result=this.userRepository.save(user);
+			
+			model.addAttribute("user", new User());
+			session.setAttribute("message", new Message("Successfully registered!", "alert-success"));
+			return "signup";
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			model.addAttribute("user",user);
+			session.setAttribute("message", new Message("Something went wrong!"+e.getMessage(), "alert-danger"));
+			return "signup";
+		}
+		
+	}
+	
+
+	//handler for custom login
+	@GetMapping("/signin")
+	public String customLogin(Model model) {
+		model.addAttribute("title","Login Page");
+		return "login";
+	}
+}
+
+
+
